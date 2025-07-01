78210a86b0e034b4d91d60cbf34a5f7d1959d38a
laniatax <89772253+laniatax@users.noreply.github.com>
2025-07-01 10:15:16 +0500

    Update HW_1.py

--- HW_1.py
+++ HW_1.py
@@ -1,12 +1,12 @@
 import torch
 
-# 1.1 Создание тензоров
+# Задание 1.1 Создание тензоров
 tensor1 = torch.rand(3, 4)                     # Случайные числа от 0 до 1
 tensor2 = torch.zeros(2, 3, 4)                 # Нули
 tensor3 = torch.ones(5, 5)                     # Единицы
 tensor4 = torch.arange(16).reshape(4, 4)       # От 0 до 15
 
-# 1.2 Операции с тензорами
+# Задание 1.2 Операции с тензорами
 A = torch.rand(3, 4)
 B = torch.rand(4, 3)
 A_T = A.T                                      # Транспонирование
@@ -14,14 +14,14 @@ matmul_result = torch.matmul(A, B)             # Матричное умноже
 elemwise_mult = A * B.T                        # Поэлементное умножение
 sum_A = A.sum()                                # Сумма всех элементов
 
-# 1.3 Индексация и срезы
+# Задание 1.3 Индексация и срезы
 tensor = torch.rand(5, 5, 5)
 first_row = tensor[0, :, :]                    # Первая строка
 last_column = tensor[:, :, -1]                 # Последний столбец
 center = tensor[2:4, 2:4, 2:4]                 # Центр 2x2x2
 even_indices = tensor[::2, ::2, ::2]           # Четные индексы
 
-# 1.4 Работа с формами
+# Задание 1.4 Работа с формами
 flat_tensor = torch.arange(24)
 reshape_2x12 = flat_tensor.reshape(2, 12)
 reshape_3x8 = flat_tensor.reshape(3, 8)
@@ -32,7 +32,7 @@ reshape_2x2x2x3 = flat_tensor.reshape(2, 2, 2, 3)
 
 import torch
 
-# 2.1 Градиенты простой функции
+# Задание 2.1 Градиенты простой функции
 x = torch.tensor(1.0, requires_grad=True)
 y = torch.tensor(2.0, requires_grad=True)
 z = torch.tensor(3.0, requires_grad=True)
@@ -40,7 +40,7 @@ f = x**2 + y**2 + z**2 + 2 * x * y * z
 f.backward()
 # Градиенты: df/dx = 2x + 2yz, аналогично для y и z
 
-# 2.2 MSE и градиенты
+# Задание 2.2 MSE и градиенты
 def mse_loss(y_pred, y_true):
     return ((y_pred - y_true) ** 2).mean()
 
@@ -54,13 +54,12 @@ loss = mse_loss(y_pred, y_true)
 loss.backward()
 # Градиенты: w.grad, b.grad
 
-# 2.3 Цепное правило
+# Задание 2.3 Цепное правило
 x = torch.tensor(1.0, requires_grad=True)
 f = torch.sin(x**2 + 1)
 f.backward()
 # df/dx = cos(x^2 + 1) * 2x
 
-# Проверка через grad
 from torch.autograd import grad
 grad_f = grad(f, x)[0]
 
@@ -71,12 +70,12 @@ import time
 device_cpu = torch.device("cpu")
 device_gpu = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# 3.1 Подготовка данных
+# Заание 3.1 Подготовка данных
 shapes = [(64, 1024, 1024), (128, 512, 512), (256, 256, 256)]
 tensors_cpu = [torch.rand(s) for s in shapes]
 tensors_gpu = [t.to(device_gpu) for t in tensors_cpu]
 
-# 3.2 Функция времени
+# Задание 3.2 Функция времени
 def measure_time_cpu(func, *args):
     start = time.time()
     func(*args)
@@ -91,7 +90,7 @@ def measure_time_gpu(func, *args):
     torch.cuda.synchronize()
     return start.elapsed_time(end)
 
-# 3.3 Сравнение
+# Задание 3.3 Сравнение
 def run_benchmark():
     operations = {
         "Матмул": lambda x: torch.matmul(x, x.transpose(-1, -2)),
